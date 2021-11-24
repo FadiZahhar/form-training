ba012d925573774679762a80f78176bdbf17f005
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-24 11:46:54 +0200

    introducing ref

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,56 +1,39 @@
-import React from 'react'
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import styles from './App.module.css';
-import { useForm } from "react-hook-form"
-import { useState } from 'react';
-function App() {
 
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
 
-  const nameHandler = event => {
-    const name = event.target.value;
+class App extends React.Component {
 
-    setName(name);
-  }
-  const passwordHandler = event => {
-    const password = event.target.value;
-
-    setPassword(password);
-  }
-  const formHandler = event => {
+  handleSubmit = event => {
     event.preventDefault();
 
+    const name = this.nameRef.value
+
     alert(name)
-    alert(password)
   }
-  return (
-    <form className={styles.container} onSubmit={event => formHandler(event)} >
-      <div className={styles.form_group}>
-        <input
-          name="name"
-          type="text"
-          value={name}
-          placeholder="Name"
-          className={styles.form_control}
-          onChange={event => nameHandler(event)}
-        />
-        <input
-          name="name"
-          type="password"
-          value={password}
-          placeholder="Name"
-          className={styles.form_control}
-          onChange={event => passwordHandler(event)}
-        />
-      </div>
-      <button className={styles.button}>Submit</button>
+
+  render() {
+    return (
+      <form className={styles.container} onSubmit={this.handleSubmit}>
+        <div className={styles.form_group}>
+          <input
+            name="name"
+            type="text"
+            placeholder="Name"
+            className={styles.form_control}
+            ref={input => this.nameRef = input}
+          />
+
+        </div>
+        <button className={styles.button}>Submit</button>
 
 
-    </form>
+      </form>
 
 
-  );
+    );
+  }
+
 }
 
 export default App;
