44eff112c6c502f75e3d015cc505f1903fc35d71
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-24 13:38:07 +0200

    Introducing control input

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,97 +1,65 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './App.module.css';
-import CustomInput from './CustomInput/CustomInput.component';
-import CustomSelect from './CustomSelect/CustomSelect.component';
+import { useState } from 'react';
 
 
-class App extends React.Component {
+function App() {
 
-  handleSubmit = event => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const nameHandler = event => {
+    const name = event.target.value
+
+    setName(name)
+  }
+  const passwordHandler = event => {
+    const password = event.target.value
+
+    setPassword(password)
+  }
+
+  const formHandler = event => {
     event.preventDefault();
 
-    const name = this.nameRef.value
-    const password = this.passwordRef.value
-    const gender = this.genderRef.value
-    const pet = this.form.pet.value
+    alert(password)
+    alert(name)
 
-    // to get out the checked inputs
-    const role = this.form.role
-    const roleArray = Array.prototype.slice.call(role)
-    const checkRoleArray = roleArray.filter(input => input.checked)
-    const checkValues = checkRoleArray.map(input => input.value)
-
-    // alert(checkValues)
-    const errors = this.validation(name, password, gender, pet, checkValues)
-    if (errors.length) {
-      alert(errors)
-      return;
-    }
-    alert('Validation successful')
   }
-  validation = (name, password, gender, pet, roles) => {
-    let errors = []
-    if (!name.length) {
-      errors.push('Name cannot be empty')
-    }
-    if (!password.length) {
-      errors.push('Password cannot be empty')
-    }
-    if (!gender.length) {
-      errors.push('Gender cannot be empty')
-    }
-    if (!pet.length) {
-      errors.push('Pet cannot be empty')
-    }
-    if (!roles.length) {
-      errors.push('Role cannot be empty')
-    }
-    return errors
-  }
-  render() {
-    return (
-      <form className={styles.container} onSubmit={this.handleSubmit} ref={form => this.form = form}>
-
-        <div className={styles.form_group}>
-          <CustomInput name="name" type="text" placeholder="Name" inputRef={input => this.nameRef = input} />
-        </div>
-
-        <div className={styles.form_group}>
-          <CustomInput name="password" type="password" placeholder="Password" inputRef={input => this.passwordRef = input} />
-        </div>
-
-        <div className={styles.form_group}>
-          <CustomSelect
-            name="gender"
-            inputRef={input => this.genderRef = input}
-            options={[
-              {
-                'label': 'Male',
-                'value': 'Male'
-              },
-              {
-                'label': 'Female',
-                'value': 'Female'
-              },
-            ]}
-          />
-        </div>
-        <div className={styles.form_group}>
-          Dog <input type="radio" name="pet" value="Dog" />
-          Cat <input type="radio" name="pet" value="Cat" />
-        </div>
-
-        <div className={styles.form_group}>
-          <input type="checkbox" name="role" value="Designer" /> Designer
-          <input type="checkbox" name="role" value="Frontend" /> Frontend
-          <input type="checkbox" name="role" value="Backend" /> Backend
-        </div>
-        <button className={styles.button}>Submit</button>
-
-      </form>
 
 
-    );
-  }
+  return (
+    <form className={styles.container} onSubmit={event => formHandler(event)} >
+
+      <div className={styles.form_group}>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          placeholder="Name"
+          className={styles.form_control}
+
+          onChange={event => nameHandler(event)}
+        />
+      </div>
+      <div className={styles.form_group}>
+        <input
+          name="password"
+          type="password"
+          value={password}
+          placeholder="password"
+          className={styles.form_control}
+
+          onChange={event => passwordHandler(event)}
+        />
+      </div>
+      <button className={styles.button}>Submit</button>
+
+    </form>
+
+
+  );
+
 
 }
 
