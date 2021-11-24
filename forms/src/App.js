e5ea1e4e7cd0c81270ffd02e847213b79d98b957
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-24 10:11:54 +0200

    creating form and using useState

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,24 +1,55 @@
+import React from 'react'
 import logo from './logo.svg';
-import './App.css';
-
+import styles from './App.module.css';
+import { useForm } from "react-hook-form"
+import { useState } from 'react';
 function App() {
+
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const nameHandler = event => {
+    const name = event.target.value;
+
+    setName(name);
+  }
+  const passwordHandler = event => {
+    const password = event.target.value;
+
+    setPassword(password);
+  }
+  const formHandler = event => {
+    event.preventDefault();
+
+    alert(name)
+    alert(password)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className={styles.container} onSubmit={event => formHandler(event)} >
+      <div className={styles.form_group}>
+        <input
+          name="name"
+          type="text"
+          value={name}
+          placeholder="Name"
+          className={styles.form_control}
+          onChange={event => nameHandler(event)}
+        />
+        <input
+          name="name"
+          type="password"
+          value={password}
+          placeholder="Name"
+          className={styles.form_control}
+          onChange={event => passwordHandler(event)}
+        />
+      </div>
+      <button className={styles.button}>Submit</button>
+
+
+    </form>
+
+
   );
 }
 
