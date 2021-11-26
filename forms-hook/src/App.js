2fa07a01c8216bde32b0ec614dafdeda7bf36441
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-26 10:34:18 +0200

    using the react-hook-form for input validation

--- forms-hook/src/App.js
+++ forms-hook/src/App.js
@@ -2,6 +2,46 @@ import React from 'react'
 import styles from './App.module.css';
 import { useForm } from "react-hook-form";
 
+
+const MyInput = ({ name, type, label, placeholder, register, required }) => {
+
+  let input = (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        {...register(label, { required })}
+        className={styles.form_control}
+      />
+    </div>
+  )
+  if (type === "textarea") {
+    input = (
+      <div>
+        <label htmlFor={name}>{label}</label>
+        <textarea
+          name={name}
+          placeholder={placeholder}
+          {...register(label, { required })}
+          className={styles.form_control}
+          cols="4"
+          rows="10"
+        ></textarea>
+      </div>
+    )
+
+  }
+  return (
+    <div>
+      {input}
+    </div>
+
+
+  )
+}
+
 function App() {
   const { handleSubmit, register, errors } = useForm();
 
@@ -16,11 +56,33 @@ function App() {
           type="text"
           placeholder="Name"
           className={styles.form_control}
-          {...register('test', { required: true })}
-
+          {...register("name")}
+          required
         />
-
       </div>
+      <div className={styles.form_group}>
+        <MyInput
+          name="password"
+          type="password"
+          label="password"
+          register={register}
+          placeholder="Password"
+          required
+        />
+      </div>
+
+      <div className={styles.form_group}>
+        <MyInput
+          name="description"
+          type="textarea"
+          label="description"
+          register={register}
+          placeholder="description"
+          required
+        />
+      </div>
+
+
       <button className={styles.button}>Submit</button>
 
 
