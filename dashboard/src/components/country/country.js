72512bc41beaa16f81c0a6958a2b7a6653c0cfd3
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-12-02 12:31:03 +0200

    Routing done

---
+++ dashboard/src/components/country/country.js
@@ -0,0 +1,16 @@
+import React, { Component} from 'react';
+
+class Country extends Component{
+
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div >
+<p>Country</p>
+            </div>
+        )
+    }
+}
+export default Country;
