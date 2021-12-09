7111f0f664c67a08aafe53da21d9e52afc2579b9
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-09 09:52:50 +0200

    Tricking React with JSDOM

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,5 +1,9 @@
 // App.test.js the test is only for clarifying its test file we can name it anything
-
+/**
+ * Notes about Test Structure
+ * The IT function is used to organize all the different tests that we write inside of a single file,
+ * it is a global function , takes two arguments one for the descroption and other for the test logic
+ */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
