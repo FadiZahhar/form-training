07b302126a8f39f8059f842eaca3b76eace90b54
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-09 14:10:10 +0200

    test on the submit

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -28,3 +28,16 @@ it('has a text area that users can type in', () => {
     expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 
 })
+
+it('when form is submitted text area got empty', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    })
+    wrapped.update();
+
+    wrapped.find('form').simulate('submit')
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+
+})
