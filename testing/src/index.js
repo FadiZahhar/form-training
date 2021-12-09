12d12010984369cbb120b66d228fd68922b41cfe
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-09 14:50:44 +0200

    The Provider Tag

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from 'reducers'
 
 import App from 'components/App'
 
 ReactDOM.render(
-    <App />,
+    // createStore take 2 arguments reducers and initial state
+    <Provider store={createStore(reducers, {})}>
+        <App />,
+    </Provider>,
+
     document.querySelector('#root')
 )
