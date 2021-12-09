82372140fdf7e3a81466ff12b19caec7d555797f
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-09 13:13:16 +0200

    Asserting Element Existence

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { mount } from 'enzyme'
 import CommentBox from 'components/CommentBox'
 
 it('has a text area and a button', () => {
+    // we can use her shallow but for practicing we used mount
+    const wrapped = mount(<CommentBox />);
+
+    // console.log(wrapped.find("textarea").length);
+    // console.log(wrapped.find("button").length);
+
+    expect(wrapped.find('textarea').length).toEqual(1)
+    expect(wrapped.find('button').length).toEqual(1)
 
 })
