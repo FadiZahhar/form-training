5e12101c1b53eedcd387cc3edd6e85b780afb7dd
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-10 14:10:39 +0200

    Creating an Event Form

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+import EventForm from "../eventForm/EventForm";
 import EventList from "./EventList";
 
 export default function EventDashboard() {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList/>
+        <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Right Column</h2>
+        <EventForm />
       </Grid.Column>
     </Grid>
   );
