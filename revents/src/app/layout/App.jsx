aad94d9a5302ce50df7cf23ed77e36e7c9199068
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-13 09:49:14 +0200

    Routing configuration

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,5 +1,9 @@
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
+import EventForm from "../../features/events/eventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 import React from "react";
 import { useState } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/Navbar";
@@ -13,17 +17,18 @@ function App() {
     setFormOpen(true);
   }
 
-  function handleCreateFormOpen(){
-    setSelectedEvent(null)
-    setFormOpen(true)
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
   }
   return (
     <>
       <NavBar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} 
-        selectEvent={handleSelectEvent}
-        selectedEvent={selectedEvent} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/events" component={EventDashboard} />
+        <Route path="/events/:id" component={EventDetailedPage} />
+        <Route path="/createEvent" component={EventForm} />
       </Container>
     </>
   );
