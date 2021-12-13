52b36e0d2d40e0b9b9ab5e3c7c4f637788e7719c
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-13 09:55:26 +0200

    Using NavLinks and Links

--- revents/src/features/nav/Navbar.jsx
+++ revents/src/features/nav/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
 
 export default function NavBar({ setFormOpen }) {
   return (
     <Menu inverted fixed="top">
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to="/" header>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 15 }} />
           Re-vents
         </Menu.Item>
-        <Menu.Item name="Events" />
-        <Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        <Menu.Item as={NavLink} to="/createEvent">
           {/* we can not write onClick={setFormOpen(true)} it will lead to ERROR because cannot update a component while rendering by using arrow function we delay it */}
           <Button
-            onClick={() => setFormOpen(true)}
+            // onClick={() => setFormOpen(true)}
             positive
             inverted
             content="Create Event"
