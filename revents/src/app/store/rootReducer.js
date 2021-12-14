6169f443166faf54d0694215faa0c3e829f50c3d
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-14 16:40:48 +0200

    Modals

--- revents/src/app/store/rootReducer.js
+++ revents/src/app/store/rootReducer.js
@@ -1,10 +1,12 @@
 import eventReducer from '../../features/events/eventReducer'
 import testReducer from '../../features/sandbox/testReducer'
 import { combineReducers } from 'redux'
+import modalReducer from '../common/modals/modalReducer'
 
 const rootReducer = combineReducers({
     test: testReducer,
-    event: eventReducer
+    event: eventReducer,
+    modals: modalReducer
 })
 
 export default rootReducer
