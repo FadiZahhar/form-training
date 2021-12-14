a78a4a554b6ea3b5d0fcdaa97efde5b0c3b7addf
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-14 14:24:50 +0200

    Creating a reusable text input

--- revents/src/features/events/eventForm/EventForm.jsx
+++ revents/src/features/events/eventForm/EventForm.jsx
@@ -7,6 +7,7 @@ import { useDispatch } from 'react-redux';
 import { createEvent, updateEvent } from '../eventActions';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import MyTextInput from './../../../app/common/form/MyTextInput';
 
 export default function EventForm({ match, history }) {
   const dispatch = useDispatch();
@@ -55,13 +56,7 @@ export default function EventForm({ match, history }) {
         onSubmit={(values) => console.log(values)}
       >
         <Form className="ui form">
-          <FormField>
-            <Field name="title" placeholder="Event title" />
-            <ErrorMessage
-              name="title"
-              render={(error) => <Label basic color="red" content={error} />}
-            />
-          </FormField>
+          <MyTextInput name="title" placeholder="Event title" />
           <FormField>
             <Field name="category" placeholder="category" />
           </FormField>
