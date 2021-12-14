5209595a4887b25c6a99d2211c4312cd19ba0a80
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-12-14 16:23:39 +0200

    Adding signin and signout components

--- reduxf/src/features/nav/NavBar.jsx
+++ reduxf/src/features/nav/NavBar.jsx
@@ -1,6 +1,8 @@
 import React  from "react";
 import { NavLink } from "react-router-dom";
 import { Menu, Container, Button} from "semantic-ui-react";
+import SignedInMenu from "./SignedinMenu";
+import SignedOutMenu from "./SignedOutMenu";
 
 export default function NavBar({setFormOpen}){
     return(
@@ -14,10 +16,9 @@ export default function NavBar({setFormOpen}){
                 <Menu.Item as={NavLink} to='/createEvent'>
                     <Button  positive inverted content='Create Event'/>
                 </Menu.Item>
-                <Menu.Item position='right'>
-                    <Button basic inverted content='Login'/>
-                    <Button basic inverted content='Register' style={{marginLeft:'0.5em'}}/>
-                </Menu.Item>
+                <SignedOutMenu/>
+                <SignedInMenu/>
+                
             </Container>
 
         </Menu>
