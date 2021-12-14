95e603a6d0fcf23110a74db4a9cfdcc29c5fbb3a
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-12-14 13:51:56 +0200

    using navlinks and links

--- reduxf/src/app/layout/App.jsx
+++ reduxf/src/app/layout/App.jsx
@@ -1,7 +1,11 @@
 import React,{useState} from 'react';
+import { Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import EventDetailedPage from '../../features/events/EventDetailed/EventDetailedPage';
+import EventForm from '../../features/events/eventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 import NavBar from '../../features/nav/NavBar';
 
 
@@ -19,19 +23,17 @@ export default function App() {
    setFormOpen(true);
  }
   return (
-    <div>
+    <>
 
-    <NavBar setFormOpen={handleCreateFormOpen}/>
+      <NavBar setFormOpen={handleCreateFormOpen}/>
       
-      <Container className='main'>
-        <EventDashboard 
-          formOpen={formOpen} 
-          setFormOpen={setFormOpen} 
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
-      </Container>
-     </div>
+        <Container className='main'>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/event' component={EventDashboard}/>
+          <Route path='/events/:id' component={EventDetailedPage}/>
+          <Route path='/createEvent' component={EventForm}/>
+        </Container>
+    </>
   );
 }
 
