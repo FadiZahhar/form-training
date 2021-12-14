8a6d493c96c25508b8d6ec6a812aa388c7d368fc
Anthony sayegh <Sayeghanthony89012@gmail.com>
2021-12-14 17:18:06 +0200

    using the useHistery

--- reduxf/src/features/nav/NavBar.jsx
+++ reduxf/src/features/nav/NavBar.jsx
@@ -1,5 +1,6 @@
 import React  from "react";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { Menu, Container, Button} from "semantic-ui-react";
 import SignedInMenu from "./SignedinMenu";
@@ -7,7 +8,12 @@ import SignedOutMenu from "./SignedOutMenu";
 
 export default function NavBar({setFormOpen}){
     const [authenticated,setAuthentication]= useState(false);
-    
+    const history = useHistory();
+
+    function handleSignOut(){
+        setAuthentication(false);
+        history.push('/');
+    }
     return(
         <Menu inverted  fixed='top'>
             <Container>
@@ -22,7 +28,7 @@ export default function NavBar({setFormOpen}){
                 </Menu.Item> 
                 }
                 {authenticated ?
-                <SignedInMenu setAuthentication={setAuthentication}/> : 
+                <SignedInMenu signOut={handleSignOut}/> : 
                 <SignedOutMenu setAuthentication={setAuthentication}/>}
                 
                 
