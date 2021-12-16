5924f5744c4f31e15ae0007288dfeb8cafde9af8
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-16 23:48:52 +0200

    Handling not found documents

--- revents/src/app/hooks/useFirestoreDoc.js
+++ revents/src/app/hooks/useFirestoreDoc.js
@@ -11,6 +11,10 @@ export default function useFirestoreDoc({ query, data, deps }) {
         dispatch(asyncActionStart());
         const unsubscribe = query().onSnapshot(
             snapshot => {
+                if (!snapshot.exists) {
+                    dispatch(asyncActionError({ code: 'not-found', message: 'Could not find document' }))
+                    return
+                }
                 data(dataFromSnapshot(snapshot))
                 dispatch(asyncActionFinish())
             },
