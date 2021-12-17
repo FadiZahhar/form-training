24d15b790dca95426569441e0029ce9d097fc5ca
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-17 11:05:41 +0200

    Logging in

--- revents/src/features/auth/authActions.js
+++ revents/src/features/auth/authActions.js
@@ -1,10 +1,14 @@
 import { SIGN_IN_USER, SIGN_OUT_USER } from "./authConstants";
+import firbase from '../../app/config/firebase';
 
-export function signInUser(payload) {
-    return {
-        type: SIGN_IN_USER,
-        // when the parameter is same word as the attribute we can make shortcut and just payload rather than payload:payload
-        payload
+export function signInUser(creds) {
+    return async function (dispatch) {
+        try {
+            const result = await firbase.auth().signInWithEmailAndPassword(creds.email, creds.password);
+            dispatch({ type: SIGN_IN_USER, payload: result.user })
+        } catch (error) {
+            throw error
+        }
     }
 }
 
