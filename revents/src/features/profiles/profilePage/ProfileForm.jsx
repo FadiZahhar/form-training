cfd1ccb8c5cd37d2dff9f93a560677d53176c89e
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-20 23:14:42 +0200

    Adding the update user actions

--- revents/src/features/profiles/profilePage/ProfileForm.jsx
+++ revents/src/features/profiles/profilePage/ProfileForm.jsx
@@ -4,6 +4,8 @@ import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
 import { Button } from 'semantic-ui-react';
 import * as Yup from 'yup';
+import { toast } from 'react-toastify';
+import { updateUserProfile } from '../../../app/firestore/firestoreService';
 
 export default function ProfileForm({ profile }) {
   return (
@@ -15,8 +17,14 @@ export default function ProfileForm({ profile }) {
       validationSchema={Yup.object({
         displayName: Yup.string().required(),
       })}
-      onSubmit={async (values) => {
-        console.log(values);
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          await updateUserProfile(values);
+        } catch (error) {
+          toast.error(error.message);
+        } finally {
+          setSubmitting(false);
+        }
       }}
     >
       {({ isSubmitting, isValid, dirty }) => (
