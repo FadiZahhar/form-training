55bc964e1ee46feacfe40babedd6ba0b042eb808
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-21 12:13:14 +0200

    Using the upload method in the widget

--- revents/src/app/common/photos/PhotoUploadWidget.jsx
+++ revents/src/app/common/photos/PhotoUploadWidget.jsx
@@ -2,10 +2,52 @@ import React, { useState } from 'react';
 import { Button, Grid, Header } from 'semantic-ui-react';
 import PhotoWidgetCropper from './PhotoWidgetCropper';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
+import cuid from 'cuid';
+import { getFileExtension } from '../util/util';
+import { uploadToFirebaseStorage } from '../../../app/firestore/firebaseService';
+import { toast } from 'react-toastify';
+import { updateUserProfilePhoto } from '../../../app/firestore/firestoreService';
 
-export default function PhotoUploadWidget() {
+export default function PhotoUploadWidget({ setEditMode }) {
   const [files, setFiles] = useState([]);
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  function handleUploadImage() {
+    setLoading(true);
+    const filename = cuid() + '.' + getFileExtension(files[0].name);
+    const uploadTask = uploadToFirebaseStorage(image, filename);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        toast.error(error.message);
+      },
+      () => {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          updateUserProfilePhoto(downloadURL, filename)
+            .then(() => {
+              setLoading(false);
+              handelCancelCrop();
+              setEditMode(false);
+            })
+            .catch((error) => {
+              toast.error(error.message);
+              setLoading(false);
+            });
+        });
+      }
+    );
+  }
+
+  function handelCancelCrop() {
+    setFiles([]);
+    setImage(null);
+  }
   return (
     <Grid>
       <Grid.Column width={4}>
@@ -32,8 +74,19 @@ export default function PhotoUploadWidget() {
               style={{ minHeight: 200, minWidth: 200, overflow: 'hidden' }}
             />
             <Button.Group>
-              <Button style={{ width: 100 }} positive icon="check" />
-              <Button style={{ width: 100 }} icon="close" />
+              <Button
+                loading={loading}
+                onClick={handleUploadImage}
+                style={{ width: 100 }}
+                positive
+                icon="check"
+              />
+              <Button
+                disabled={loading}
+                onClick={handelCancelCrop}
+                style={{ width: 100 }}
+                icon="close"
+              />
             </Button.Group>
           </>
         )}
