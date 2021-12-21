09055f8736f881ce1e38785af2fc5743299ce20c
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-21 09:41:49 +0200

    Adding a profile photos page

---
+++ revents/src/features/profiles/profilePage/PhotosTab.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Grid, Header, Button, Tab, Card, Image } from 'semantic-ui-react';
+
+export default function PhotosTab({ profile, isCurrentUser }) {
+  const [editMode, setEditMode] = useState(false);
+  return (
+    <Tab.Pane>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated="left" icon="user" content={`Photos`} />
+          {isCurrentUser && (
+            <Button
+              onClick={() => setEditMode(!editMode)}
+              floated="right"
+              basic
+              content={editMode ? 'Cancel' : 'Add Photo'}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {editMode ? (
+            <p>Photo widget will go here</p>
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              <Card>
+                <Image src={'/assets/user.png'} />
+                <Button.Group fluid widths={2}>
+                  <Button basic color="green" content="Main" />
+                  <Button basic color="red" icon="trash" />
+                </Button.Group>
+              </Card>
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Tab.Pane>
+  );
+}
