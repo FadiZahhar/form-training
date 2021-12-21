29381a5f3158a33bb2d8ef133829d350392e6762
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-21 18:53:14 +0200

    Adding the user event query

--- revents/src/app/firestore/firestoreService.js
+++ revents/src/app/firestore/firestoreService.js
@@ -168,6 +168,25 @@ export async function cancelUserAttendance(event) {
             attendees: eventDoc.data().attendees.filter(attendee => attendee.id !== user.uid)
         })
     } catch (error) {
+        throw error
+    }
+}
 
+export function getUserEventsQuery(activeTab, userUid) {
+    let eventsRef = db.collection('events');
+    const today = new Date();
+    switch (activeTab) {
+        case 1: // past events
+            return eventsRef
+                .where('attendeeIds', 'array-contains', userUid)
+                .where('date', '<=', today)
+                .orderBy('date', 'desc');
+        case 2: // hosting
+            return eventsRef.where('hostUid', '==', userUid).orderBy('date');
+        default:
+            return eventsRef
+                .where('attendeeIds', 'array-contains', userUid)
+                .where('date', '>=', today)
+                .orderBy('date');
     }
 }
