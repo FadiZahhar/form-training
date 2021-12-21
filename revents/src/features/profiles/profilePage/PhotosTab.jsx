81ff5f4a0fb1fb42b0ed62370696525a9b03fb40
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-21 12:24:38 +0200

    Displaying the images

--- revents/src/features/profiles/profilePage/PhotosTab.jsx
+++ revents/src/features/profiles/profilePage/PhotosTab.jsx
@@ -1,11 +1,24 @@
 import PhotoUploadWidget from '../../../app/common/photos/PhotoUploadWidget';
 import React, { useState } from 'react';
 import { Grid, Header, Button, Tab, Card, Image } from 'semantic-ui-react';
+import useFirestoreCollection from '../../../app/hooks/useFirestoreCollection';
+import { getUserPhotos } from '../../../app/firestore/firestoreService';
+import { useDispatch, useSelector } from 'react-redux';
+import { listentoUserPhotos } from '../profileAction';
 
 export default function PhotosTab({ profile, isCurrentUser }) {
-  const [editMode, setEditMode] = useState(true);
+  const dispatch = useDispatch();
+  const [editMode, setEditMode] = useState(false);
+  const { loading } = useSelector((state) => state.async);
+  const { photos } = useSelector((state) => state.profile);
+
+  useFirestoreCollection({
+    query: () => getUserPhotos(profile.id),
+    data: (photos) => dispatch(listentoUserPhotos(photos)),
+    deps: [profile.id, dispatch],
+  });
   return (
-    <Tab.Pane>
+    <Tab.Pane loading={loading}>
       <Grid>
         <Grid.Column width={16}>
           <Header floated="left" icon="user" content={`Photos`} />
@@ -23,13 +36,15 @@ export default function PhotosTab({ profile, isCurrentUser }) {
             <PhotoUploadWidget setEditMode={setEditMode} />
           ) : (
             <Card.Group itemsPerRow={5}>
-              <Card>
-                <Image src={'/assets/user.png'} />
-                <Button.Group fluid widths={2}>
-                  <Button basic color="green" content="Main" />
-                  <Button basic color="red" icon="trash" />
-                </Button.Group>
-              </Card>
+              {photos.map((photo) => (
+                <Card key={photo.id}>
+                  <Image src={photo.url} />
+                  <Button.Group fluid widths={2}>
+                    <Button basic color="green" content="Main" />
+                    <Button basic color="red" icon="trash" />
+                  </Button.Group>
+                </Card>
+              ))}
             </Card.Group>
           )}
         </Grid.Column>
