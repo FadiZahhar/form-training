d5eb5e190f5105d856cbbd2842057fe1acdb29ba
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-12-24 16:28:27 +0200

    Infinite scroll

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import { Button, Grid, Loader } from 'semantic-ui-react';
 import EventList from './EventList';
 import { useDispatch, useSelector } from 'react-redux';
 import EventListItemPlaceholder from './EventListItemPlaceholder';
@@ -51,14 +51,11 @@ export default function EventDashboard() {
             <EventListItemPlaceholder />
           </>
         )}
-        <EventList events={events} />
-        <Button
+        <EventList
+          events={events}
+          getNextEvents={handleFetchNextEvents}
           loading={loading}
-          disabled={!moreEvents}
-          onClick={handleFetchNextEvents}
-          color="green"
-          content="More..."
-          floated="right"
+          moreEvents={moreEvents}
         />
       </Grid.Column>
       <Grid.Column width={6}>
@@ -69,6 +66,9 @@ export default function EventDashboard() {
           setPredicate={handleSetPredicate}
           loading={loading}
         />
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Loader active={loading} />
       </Grid.Column>
     </Grid>
   );
